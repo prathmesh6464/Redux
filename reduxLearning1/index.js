a56c5ebfc6a208b36e2f3efbafc52ce9ad49437b
Prathamesh Jadhav <prathmesh6464@gmail.com>
2024-12-27 02:06:36 +0530

    Add : combineReduceres code.

--- reduxLearning1/index.js
+++ reduxLearning1/index.js
@@ -1,24 +1,45 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import logger from "redux-logger";
 import { thunk } from "redux-thunk";
 import axios from "axios";
 
-const store = createStore(reducer, applyMiddleware(logger.default, thunk));
+const store = createStore(
+  combineReducers({
+    amount: reducerAmount,
+    point: reducerPoint,
+  }),
+  applyMiddleware(logger.default, thunk)
+);
 
-function reducer(state = { amount: 1 }, action) {
+function reducerAmount(state = { amount: 1 }, action) {
   switch (action.type) {
     case "init":
       return { amount: state.amount + action.payload };
-    case "inc":
+    case "incAmt":
       return { amount: state.amount + action.payload };
     default:
       return state;
   }
 }
 
+function reducerPoint(state = { point: 100 }, action) {
+  switch (action.type) {
+    case "init":
+      return { point: state.point + action.payload };
+    case "incPnt":
+      return { point: state.point + action.payload };
+    default:
+      return state;
+  }
+}
+
 //Action creators
-function increment() {
-  return { type: "inc", payload: 2 };
+function incrementAmount() {
+  return { type: "incAmt", payload: 2 };
+}
+
+function incrementPoint() {
+  return { type: "incPnt", payload: -2 };
 }
 
 async function init(dispatch, getState) {
@@ -30,5 +51,5 @@ store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch(increment());
-store.dispatch(init);
+store.dispatch(incrementAmount());
+store.dispatch(incrementPoint());
